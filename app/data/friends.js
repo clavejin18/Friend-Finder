f28d28d57b9ab996cb5bb71a13c88ea8eb1da782
Jaime Perez <clavejin18@gmail.com>
2019-06-02 09:28:14 -0700

    changes

---
+++ app/data/friends.js
@@ -0,0 +1,21 @@
+
+var friends = [
+    {
+        "name": "mySelf :P",
+        "photo": "https://media.wired.com/photos/598e35994ab8482c0d6946e0/master/w_2400,c_limit/phonepicutres-TA.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    }
+]
+
+module.exports = friends;
